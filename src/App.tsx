cd5ef9d2fefa67603d5a60e0b9e9e700f14e5c67
To Hai Nam <namthit98@gmail.com>
2019-09-23 21:15:11 +0700

    Finish config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react'
 
 const App: React.FC = () => {
-  return <div>HELLO</div>;
-};
+  return <div>HELLO</div>
+}
 
-export default App;
+export default App
