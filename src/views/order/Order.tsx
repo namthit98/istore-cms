b3de1c63150babc4d415bcf417f4135316317e1e
Namthit98 <namthit98@gmail.com>
2019-09-26 23:29:20 +0700

    update content

--- src/views/order/Order.tsx
+++ src/views/order/Order.tsx
@@ -1,7 +1,36 @@
 import React from 'react'
 
 const Order = () => {
-  return <div>ORDERRRR</div>
+  return (
+    <div>
+      {' '}
+      <h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus minus repellat doloribus
+        animi adipisci soluta, explicabo reprehenderit esse veniam, fugit nobis consectetur nemo
+        voluptas sed asperiores, eligendi quam molestias quos!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus minus repellat doloribus
+        animi adipisci soluta, explicabo reprehenderit esse veniam, fugit nobis consectetur nemo
+        voluptas sed asperiores, eligendi quam molestias quos!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus minus repellat doloribus
+        animi adipisci soluta, explicabo reprehenderit esse veniam, fugit nobis consectetur nemo
+        voluptas sed asperiores, eligendi quam molestias quos!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus minus repellat doloribus
+        animi adipisci soluta, explicabo reprehenderit esse veniam, fugit nobis consectetur nemo
+        voluptas sed asperiores, eligendi quam molestias quos!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus minus repellat doloribus
+        animi adipisci soluta, explicabo reprehenderit esse veniam, fugit nobis consectetur nemo
+        voluptas sed asperiores, eligendi quam molestias quos!
+      </h1>
+    </div>
+  )
 }
 
 export default Order
--- src/views/product/Product.tsx
+++ src/views/product/Product.tsx
@@ -1,7 +1,86 @@
 import React from 'react'
 
 const Product = () => {
-  return <div>PRODUCTTTTT</div>
+  return (
+    <div>
+      {' '}
+      <h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus minus repellat doloribus
+        animi adipisci soluta, explicabo reprehenderit esse veniam, fugit nobis consectetur nemo
+        voluptas sed asperiores, eligendi quam molestias quos!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus minus repellat doloribus
+        animi adipisci soluta, explicabo reprehenderit esse veniam, fugit nobis consectetur nemo
+        voluptas sed asperiores, eligendi quam molestias quos!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus minus repellat doloribus
+        animi adipisci soluta, explicabo reprehenderit esse veniam, fugit nobis consectetur nemo
+        voluptas sed asperiores, eligendi quam molestias quos!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus minus repellat doloribus
+        animi adipisci soluta, explicabo reprehenderit esse veniam, fugit nobis consectetur nemo
+        voluptas sed asperiores, eligendi quam molestias quos!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus minus repellat doloribus
+        animi adipisci soluta, explicabo reprehenderit esse veniam, fugit nobis consectetur nemo
+        voluptas sed asperiores, eligendi quam molestias quos!
+      </h1>{' '}
+      <h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus minus repellat doloribus
+        animi adipisci soluta, explicabo reprehenderit esse veniam, fugit nobis consectetur nemo
+        voluptas sed asperiores, eligendi quam molestias quos!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus minus repellat doloribus
+        animi adipisci soluta, explicabo reprehenderit esse veniam, fugit nobis consectetur nemo
+        voluptas sed asperiores, eligendi quam molestias quos!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus minus repellat doloribus
+        animi adipisci soluta, explicabo reprehenderit esse veniam, fugit nobis consectetur nemo
+        voluptas sed asperiores, eligendi quam molestias quos!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus minus repellat doloribus
+        animi adipisci soluta, explicabo reprehenderit esse veniam, fugit nobis consectetur nemo
+        voluptas sed asperiores, eligendi quam molestias quos!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus minus repellat doloribus
+        animi adipisci soluta, explicabo reprehenderit esse veniam, fugit nobis consectetur nemo
+        voluptas sed asperiores, eligendi quam molestias quos!
+      </h1>{' '}
+      <h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus minus repellat doloribus
+        animi adipisci soluta, explicabo reprehenderit esse veniam, fugit nobis consectetur nemo
+        voluptas sed asperiores, eligendi quam molestias quos!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus minus repellat doloribus
+        animi adipisci soluta, explicabo reprehenderit esse veniam, fugit nobis consectetur nemo
+        voluptas sed asperiores, eligendi quam molestias quos!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus minus repellat doloribus
+        animi adipisci soluta, explicabo reprehenderit esse veniam, fugit nobis consectetur nemo
+        voluptas sed asperiores, eligendi quam molestias quos!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus minus repellat doloribus
+        animi adipisci soluta, explicabo reprehenderit esse veniam, fugit nobis consectetur nemo
+        voluptas sed asperiores, eligendi quam molestias quos!
+      </h1>
+      <h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus minus repellat doloribus
+        animi adipisci soluta, explicabo reprehenderit esse veniam, fugit nobis consectetur nemo
+        voluptas sed asperiores, eligendi quam molestias quos!
+      </h1>
+    </div>
+  )
 }
 
 export default Product
