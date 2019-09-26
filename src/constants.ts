294577584e8c7561c1652d443d120833dcca9fad
To Hai Nam <tohainam@Tos-MacBook-Pro.local>
2019-09-26 14:19:57 +0700

    update config

---
+++ src/constants.ts
@@ -0,0 +1,1 @@
+export const SERVER_URL: string = process.env.REACT_APP_SERVER_URL || '' // eslint-disable-line
--- src/libs/agent.ts
+++ src/libs/agent.ts
@@ -1,8 +1,7 @@
 import axios, { AxiosRequestConfig, AxiosInstance } from 'axios'
 import { runInAction } from 'mobx'
 import { RootStore } from '../store/root.store'
-
-const WORK_URL = 'https://5d84ee69baffda001476b595.mockapi.io/api'
+import { SERVER_URL } from '../constants'
 
 export class Agent {
   rootStore!: RootStore
@@ -13,16 +12,22 @@ export class Agent {
   }
   agent: AxiosInstance = axios.create()
   get<T = any>(endpointUrl: string, config?: AxiosRequestConfig): Promise<T> {
-    return this.agent.get<T>(WORK_URL + endpointUrl, config).then(response => response.data)
+    return this.agent.get<T>(`${SERVER_URL}${endpointUrl}`, config).then(response => response.data)
   }
   post<T = any>(endpointUrl: string, data?: any, config?: AxiosRequestConfig): Promise<T> {
-    return this.agent.post<T>(WORK_URL + endpointUrl, data, config).then(response => response.data)
+    return this.agent
+      .post<T>(`${SERVER_URL}${endpointUrl}`, data, config)
+      .then(response => response.data)
   }
   put<T = any>(endpointUrl: string, data?: any, config?: AxiosRequestConfig): Promise<T> {
-    return this.agent.put<T>(WORK_URL + endpointUrl, data, config).then(response => response.data)
+    return this.agent
+      .put<T>(`${SERVER_URL}${endpointUrl}`, data, config)
+      .then(response => response.data)
   }
   delete<T = any>(endpointUrl: string, config?: AxiosRequestConfig): Promise<T> {
-    return this.agent.delete<T>(WORK_URL + endpointUrl, config).then(response => response.data)
+    return this.agent
+      .delete<T>(`${SERVER_URL}${endpointUrl}`, config)
+      .then(response => response.data)
   }
   // authenticationReact = reaction(
   //   () => {
